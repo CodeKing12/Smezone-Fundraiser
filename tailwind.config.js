ecfbbe9e8c17a8bac99802fe6546261d84d3d576
Eyetu Kingsley <eyetukingsley330@gmail.com>
2022-05-10 17:34:12 +0000

    I am done with implementing the first page. Next up is to make it responsive.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,13 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
-
 module.exports = {
   content: ["./sources/**/*.{html,js}"],
   theme: {
     extend: {
       fontFamily: {
         nunito: ['Nunito', 'sans-serif'],
-        montserrat: ['Montserrat Alternates', 'serif'],
+        montserrat: ['Montserrat Alternates', 'serif']
       },
       'colors': {
-        'brand-green': '#C0DF00',
-      },
-      screens: {
-        'xxs': '350px',
-        'xs': '520px',
-        ...defaultTheme.screens,
+        'brand-green': '#C0DF00'
       }
     },
   },
