36185884bb620c1ccf1ad22b713be9d573c8fe34
Eyetu Kingsley <eyetukingsley330@gmail.com>
2022-05-11 17:29:41 +0000

    I am done with making most of the page responsive, styling the forms, making the form multistep, and styling the multistep form.

--- sources/js/index.js
+++ sources/js/index.js
@@ -15,3 +15,100 @@ function closeMenu() {
     document.querySelector(".menu-icon").setAttribute("onclick", "openMenu()");
     document.querySelector("body").style.overflow = "scroll";
 }
+
+
+var currentTab = 0; // Current tab is set to be the first tab (0)
+showTab(currentTab); // Display the current tab
+
+function showTab(n) {
+  // This function will display the specified tab of the form ...
+  var x = document.getElementsByClassName("tab");
+  x[n].style.display = "flex";
+  // ... and fix the Previous/Next buttons:
+  if (n == 0) {
+    document.getElementById("prevBtn").style.display = "none";
+  } else {
+    document.getElementById("prevBtn").style.display = "inline";
+  }
+  if (n == (x.length - 1)) {
+    document.getElementById("nextBtn").innerHTML = "Submit";
+  } else {
+    document.getElementById("nextBtn").innerHTML = "Next";
+  }
+
+  document.querySelector(".page-id").innerHTML = "Page " + n+1
+  // ... and run a function that displays the correct step indicator:
+  fixStepIndicator(n)
+}
+
+function nextPrev(n) {
+  // This function will figure out which tab to display
+  var x = document.getElementsByClassName("tab");
+  // Exit the function if any field in the current tab is invalid:
+  if (n == 1 && !validateForm()) return false;
+  // Hide the current tab:
+  x[currentTab].style.display = "none";
+  // Increase or decrease the current tab by 1:
+  currentTab = currentTab + n;
+  // if you have reached the end of the form... :
+  if (currentTab >= x.length) {
+    //...the form gets submitted:
+    document.getElementById("regForm").submit();
+    return false;
+  }
+  // Otherwise, display the correct tab:
+  showTab(currentTab);
+}
+
+function validateForm() {
+  // This function deals with validation of the form fields
+  var x, y, a, b, c, i, valid = true;
+  x = document.getElementsByClassName("tab");
+  a = x[currentTab].getElementsByTagName("input");
+  b = x[currentTab].getElementsByTagName("select");
+  c = x[currentTab].getElementsByTagName("textarea");
+  a_array = Array.from(a)
+  b_array = Array.from(b)
+  c_array = Array.from(c)
+  y = a_array.concat(b_array, c_array)
+
+  // A loop that checks every input field in the current tab:
+  for (i = 0; i < y.length; i++) {
+    // If a field is empty...
+    if (y[i].value == "") {
+      // add an "invalid" class to the field:
+      y[i].className += " invalid";
+      // and set the current valid status to false:
+      valid = false;
+    } else {
+        y[i].classList.remove("invalid");
+    }
+    if (y[i].tagName == "SELECT") {
+        if (y[i].value == y[i].options[0].value) {
+            y[i].className += " invalid";
+            valid = false;
+        } else {
+            y[i].classList.remove("invalid");
+        }
+    }
+  }
+  // If the valid status is true, mark the step as finished and valid:
+  if (valid) {
+    document.getElementsByClassName("step")[currentTab].className += " finish";
+    for (i = 0; i < y.length; i++) {
+        y[i].classList.remove("invalid");
+    }
+  }
+  console.log(valid)
+  return valid; // return the valid status
+}
+
+function fixStepIndicator(n) {
+  // This function removes the "active" class of all steps...
+  var i, x = document.getElementsByClassName("step");
+  for (i = 0; i < x.length; i++) {
+    x[i].className = x[i].className.replace(" active", "");
+  }
+  //... and adds the "active" class to the current step:
+  x[n].className += " active";
+}
